d79d25afa59462c25be3b5321da9e55dfe387681
Vovababiichuk <babihuk888@gmail.com>
2023-11-01 22:39:56 +0200

    api

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,37 @@
 import type { Metadata } from "next";
+import Link from "next/link";
+import { MdLocalPostOffice } from "react-icons/md";
+
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return res.json();
+}
 
 export const metadata: Metadata = {
   title: "Blog",
   description: "My Blog",
 };
 
-export default function Blog() {
-	return (
-		<div className="container">
-			<h1 className="text-3xl font-bold text-blue-600 p-10">Blog Page</h1>
-		</div>
-	)
+export default async function Blog() {
+  const posts = await getData();
+
+  return (
+    <div className="container">
+      <h1 className="text-3xl font-bold text-blue-600 p-10">Blog Page</h1>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.id}>
+            <Link href={`/blog/${post.id}`}>
+              <div className="flex items-center gap-2">
+                <span>
+									<MdLocalPostOffice />
+								</span>
+              	<span>{post.title}</span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
